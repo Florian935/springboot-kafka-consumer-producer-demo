97e406b9a2521eb89e61c4495ca79913857f13fe
user2 <florian.martin63000@gmail.com>
2020-12-01 00:39:05 +0100

    Initial commit

---
+++ src/test/java/com/training/kafka/springbootWithKafka/SpringBootWithKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.training.kafka.springbootWithKafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWithKafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
